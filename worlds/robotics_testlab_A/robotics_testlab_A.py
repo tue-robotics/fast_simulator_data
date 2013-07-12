8d9b56378ed630d3b1b3f24fb3ae4d59eeef773a
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2013-07-12 11:52:57 +0000

    Updated models and total lab pgm
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/data/data-pkgs/fast_simulator_data@63 8d03b67f-85ac-43d4-bce4-d4c6b61a0c14

---
+++ worlds/robotics_testlab_A/robotics_testlab_A.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import roslib; roslib.load_manifest('fast_simulator')
+import rospy
+
+from fast_simulator import client
+
+if __name__ == "__main__":
+    rospy.init_node('fast_simulator_object_spawner')
+
+    W = client.SimWorld()
+
+    table1 = W.add_object("table-1", "table_120x80x76", 0.052, -1.403, 0)
+    table2 = W.add_object("table-2", "table_120x80x76", 1.941, -1.403, 0)
+    bed = W.add_object("bed-1", "hospital_bed", 5.0, -1.52, 0)
