b2e4636ef01ef7e9dfac1db16c2f02b85d443dda
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2013-07-23 14:01:23 +0000

    Added coke to robotics_testlab_A world
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/data/data-pkgs/fast_simulator_data@67 8d03b67f-85ac-43d4-bce4-d4c6b61a0c14

--- worlds/robotics_testlab_A/robotics_testlab_A.py
+++ worlds/robotics_testlab_A/robotics_testlab_A.py
@@ -12,3 +12,5 @@ if __name__ == "__main__":
     table1 = W.add_object("table-1", "table_120x80x76", 0.052, -1.403, 0)
     table2 = W.add_object("table-2", "table_120x80x76", 1.941, -1.403, 0)
     bed = W.add_object("bed-1", "hospital_bed", 5.0, -1.52, 0)
+
+    coke1 = W.add_object("coke-1", "coke", 1.941, -1.403, 0.82)
