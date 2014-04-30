dc260131bbdacef800514b17b4c133fb3507b26f
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-04-30 20:46:55 +0000

    Added fast_simulator world for robotics_testlab_B
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/data/data-pkgs/fast_simulator_data@174 8d03b67f-85ac-43d4-bce4-d4c6b61a0c14

---
+++ worlds/robotics_testlab_B/robotics_testlab_B.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import roslib; roslib.load_manifest('fast_simulator')
+import rospy
+
+from fast_simulator import client
+
+if __name__ == "__main__":
+    rospy.init_node('fast_simulator_object_spawner')
+
+    W = client.SimWorld()
+
+    #table1 = W.add_object("table-1", "table_120x80x76", 0.052, -1.403, 0)
+    #table2 = W.add_object("table-2", "table_120x80x76", 1.941, -1.403, 0)
+    #bed = W.add_object("bed-1", "hospital_bed", 5.0, -1.52, 0)
+
+    #coke1 = W.add_object("coke-1", "coke", 1.941, -1.403, 0.82)
+    #qr_code1 = W.add_object("qr-code-1", "qr_code", 0.37, -0.41, 1.0)
