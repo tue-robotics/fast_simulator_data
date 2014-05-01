a191d00cf44c8b95e2478f1a95dfca2c6c692095
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-05-01 07:29:14 +0000

    Better object names
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/data/data-pkgs/fast_simulator_data@178 8d03b67f-85ac-43d4-bce4-d4c6b61a0c14

--- worlds/robotics_testlab_B/robotics_testlab_B.py
+++ worlds/robotics_testlab_B/robotics_testlab_B.py
@@ -9,11 +9,12 @@ if __name__ == "__main__":
 
     W = client.SimWorld()
 
-    table1 = W.add_object("table-1", "table_120x80x76", 3.174, -0.523, 0, 0, 0, 1.57)
-    table2 = W.add_object("table-2", "table_120x80x76", 2.120, -3.010, 0, 0, 0, 1.57)
-    table3 = W.add_object("table-3", "table_120x80x76", 1.150, -3.958, 0, 0, 0, 1.57)
-    table4 = W.add_object("table-4", "table_120x80x76", 1.150, -2.752, 0, 0, 0, 1.57)
-    table5 = W.add_object("table-5", "table_120x80x76", 2.16, -4.16, 0)
+    W.add_object("dinner_table", "table_120x80x76", 3.174, -0.523, 0, 0, 0, 1.57)
+    W.add_object("workbench", "table_120x80x76", 2.120, -3.010, 0, 0, 0, 1.57)
+    W.add_object("supply_table", "table_120x80x76", 2.16, -4.16, 0)
+    W.add_object("desk", "table_120x80x76", 1.150, -2.752, 0, 0, 0, 1.57)
+    W.add_object("desk-2", "table_120x80x76", 1.150, -3.958, 0, 0, 0, 1.57)
+
     #bed = W.add_object("bed-1", "hospital_bed", 5.0, -1.52, 0)
 
     #coke1 = W.add_object("coke-1", "coke", 1.941, -1.403, 0.82)
