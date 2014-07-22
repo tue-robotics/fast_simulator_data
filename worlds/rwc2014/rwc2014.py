1b28c21071685ded0043a85a300c8e1b43bb2f2e
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-07-22 18:22:01 +0000

    Updated
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/data/data-pkgs/fast_simulator_data@246 8d03b67f-85ac-43d4-bce4-d4c6b61a0c14

--- worlds/rwc2014/rwc2014.py
+++ worlds/rwc2014/rwc2014.py
@@ -9,8 +9,9 @@ if __name__ == "__main__":
 
     W = client.SimWorld()
     
-    milk1 = W.add_object("milk", "coke", 8.0, 5.0, 0.82)
-    milk1 = W.add_object("pringles", "garlic_sauce", 8.0, 5.5, 0.82)
-    milk1 = W.add_object("noodles", "marmalade", 8.0, 6.0, 0.82)
-    milk1 = W.add_object("biscuits", "seven_up", 8.0, 6.5, 0.82)
+    W.add_object("milk", "coke", 8.0, 5.0, 0.82)
+    W.add_object("pringles", "garlic_sauce", 8.0, 5.5, 0.82)
+    W.add_object("noodles", "rwc2014.noodles", 8.0, 6.0, 0.82)
+    W.add_object("biscuits", "seven_up", 8.0, 6.5, 0.82)
+
 
