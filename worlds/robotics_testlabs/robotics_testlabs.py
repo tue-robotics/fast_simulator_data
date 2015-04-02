88438165809629eb67926613eeeb2240c26f9ca7
Ramon Wijnands <rayman747@hotmail.com>
2015-04-02 19:52:36 +0200

    better object positions

--- worlds/robotics_testlabs/robotics_testlabs.py
+++ worlds/robotics_testlabs/robotics_testlabs.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
 
     W = client.SimWorld()
 
-    W.add_object("coke-1", "coke",  0.856, 3.471, 0.8)
-    W.add_object("coke-2", "coke",  0.476, 3.759, 0.8)
-    W.add_object("coke-3", "coke", -0.003, 3.463, 0.8)
+    W.add_object("coke-1", "coke",  1.056, 3.471, 0.8)
+    W.add_object("coke-2", "coke",  0.676, 3.559, 0.8)
+    W.add_object("coke-3", "coke",  0.303, 3.413, 0.8)
     W.add_object("coke-4", "coke",  0.676, 1.906, 0.9)
 
