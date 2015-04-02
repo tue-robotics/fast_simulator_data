c984f5d19d1fed51faf9e621c250e8a05a43cdb9
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2015-04-02 19:41:13 +0200

    Moved objects in simulator

--- worlds/robotics_testlabs/robotics_testlabs.py
+++ worlds/robotics_testlabs/robotics_testlabs.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
 
     W = client.SimWorld()
 
-    W.add_object("coke-1", "coke",  0.856, 3.671, 0.8)
-    W.add_object("coke-2", "coke",  0.476, 3.659, 0.8)
-    W.add_object("coke-3", "coke", -0.003, 3.663, 0.8)
+    W.add_object("coke-1", "coke",  0.856, 3.471, 0.8)
+    W.add_object("coke-2", "coke",  0.476, 3.759, 0.8)
+    W.add_object("coke-3", "coke", -0.003, 3.463, 0.8)
     W.add_object("coke-4", "coke",  0.676, 1.906, 0.9)
 
