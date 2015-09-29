965591452428de7fed6a057c7a8c2141663c19de
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-09-29 20:56:34 +0200

    Added some objects to the test lab

--- worlds/robotics_testlabs/robotics_testlabs.py
+++ worlds/robotics_testlabs/robotics_testlabs.py
@@ -9,6 +9,11 @@ if __name__ == "__main__":
 
     W = client.SimWorld()
 
+    W.add_object("coke-5", "sim-coke",  3.196, 4.652, 0.87)
+    W.add_object("coke-6", "sim-coke",  3.214, 4.050, 0.87)
+    W.add_object("coke-7", "sim-coke",  3.463, 4.353, 0.87)
+
+
     W.add_object("coke-1", "sim-coke",  1.056, 3.471, 0.8)
     W.add_object("coke-2", "sim-coke",  0.676, 3.559, 0.8)
     W.add_object("coke-3", "sim-coke",  0.303, 3.413, 0.8)
