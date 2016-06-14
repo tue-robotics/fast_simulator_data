7f3bee23f3504cedb1ffabb7f4fa74ec3456f15d
Rokus Ottervanger <rokusottervanger@gmail.com>
2016-06-14 15:37:00 +0200

    Add: Sim objects for blocks

--- worlds/rokus_experiment3/rokus_experiment3.py
+++ worlds/rokus_experiment3/rokus_experiment3.py
@@ -7,8 +7,12 @@ from fast_simulator import client
 if __name__ == "__main__":
     rospy.init_node('fast_simulator_object_spawner')
 
-    # W = client.SimWorld()
+    W = client.SimWorld()
 
-    # W.add_object("coke-1", "sim-coke",  5.039, 2.543, 1.1)
-    # W.add_object("coke-2", "sim-coke",  5.060, 4.084, 1.1)
-    # W.add_object("person-1", "loy", 1.770, 0.194, 0.0, 0, 0, 1)
+    W.add_object("block-1", "rokus_experiments/small_block", 0.875, -0.5, 0.0, 0.0, 0.0, 1.570796)
+    W.add_object("block-2", "rokus_experiments/small_block", 0.225,  0.5, 0.0, 0.0, 0.0, 1.570796)
+    W.add_object("block-3", "rokus_experiments/small_block", 0.625,  0.9, 0.0, 0.0, 0.0, 1.570796)
+    W.add_object("block-4", "rokus_experiments/small_block", 2.175,  1.85, 0.0, 0.0, 0.0, 0.0)
+    W.add_object("block-5", "rokus_experiments/small_block", 0.625,  2.7, 0.0, 0.0, 0.0, 1.570796)
+    W.add_object("block-6", "rokus_experiments/small_block", 0.225,  0.5, 0.0, 0.0, 0.0, 1.570796)
+    W.add_object("block-7", "rokus_experiments/small_block", -1.025,  1.85, 0.0, 0.0, 0.0, 0.0)
