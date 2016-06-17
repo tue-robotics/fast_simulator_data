afe46a63dcdc8507641f542b90be8b8f39b3fe9c
Rokus Ottervanger <rokusottervanger@gmail.com>
2016-06-17 16:31:01 +0200

    Remove: double block

--- worlds/rokus_experiment3/rokus_experiment3.py
+++ worlds/rokus_experiment3/rokus_experiment3.py
@@ -14,5 +14,4 @@ if __name__ == "__main__":
     W.add_object("block-3", "rokus_experiments/small_block", 0.625,  0.9, 0.0, 0.0, 0.0, 1.570796)
     W.add_object("block-4", "rokus_experiments/small_block", 2.175,  1.85, 0.0, 0.0, 0.0, 0.0)
     W.add_object("block-5", "rokus_experiments/small_block", 0.625,  2.7, 0.0, 0.0, 0.0, 1.570796)
-    W.add_object("block-6", "rokus_experiments/small_block", 0.225,  0.5, 0.0, 0.0, 0.0, 1.570796)
-    W.add_object("block-7", "rokus_experiments/small_block", -1.025,  1.85, 0.0, 0.0, 0.0, 0.0)
+    W.add_object("block-6", "rokus_experiments/small_block", -1.025,  1.85, 0.0, 0.0, 0.0, 0.0)
