af395c3304283540dfb1477f331e8a8195f300b0
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2017-07-29 12:07:38 +0900

    more cokes in rwc2017

--- worlds/rwc2017/rwc2017.py
+++ worlds/rwc2017/rwc2017.py
@@ -23,4 +23,23 @@ if __name__ == "__main__":
     W.add_object("coke-7", "sim-coke",  0.00, -7.45, 0.71)
     W.add_object("coke-8", "sim-coke", 0.1725, -7.525, 0.71)
 
+    # left_rack
+    W.add_object("coke-9", "sim-coke", -2.35, -0.3, 0.91)
+
+    # right_rack
+    W.add_object("coke-10", "sim-coke", -0.85, -0.3, 0.91)
+
+    # kitchen_counter
+    W.add_object("coke-11", "sim-coke", 5.82, -3.7, 0.86)
+
+    # sideboard
+    W.add_object("coke-12", "sim-coke", 2.64, -0.44, 0.50)
+    W.add_object("coke-13", "sim-coke", 2.64, -0.44, 0.91)
+
+    # kitchen_shelf
+    W.add_object("coke-14", "sim-coke", 2.47, -4.15, 0.42)
+
+    # kitchen_rack
+    W.add_object("coke-15", "sim-coke", 2.43, -3.15, 0.56)
+
     W.add_object("person-1", "loy", -1.0, -9.0, 0.0)
