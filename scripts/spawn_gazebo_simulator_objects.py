94ca11ee84947adb9017eae263e82dd957a44914
Kwin van der Veen <kwinvdv@gmail.com>
2019-01-12 13:33:58 +0100

    Added more comments.

--- scripts/spawn_gazebo_simulator_objects.py
+++ scripts/spawn_gazebo_simulator_objects.py
@@ -8,7 +8,7 @@ from rospkg import RosPack
 from gazebo_msgs.srv import SpawnModel
 from geometry_msgs.msg import Pose
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # Initialize ROS node
     rospy.init_node('gazebo_simulator_object_spawner')
 
@@ -18,25 +18,26 @@ if __name__ == "__main__":
 
     # Load yaml with list of objects that need to be loaded
     package_path = RosPack().get_path('fast_simulator_data')
-    with open(package_path + "/sdf_test/objects_list_test2.yaml", 'r') as f:
+    with open(package_path + '/sdf_test/objects_list_test2.yaml', 'r') as f:
         data = yaml.safe_load(f)
 
-    # Get paths to $GAZEBO_MODEL_PATH
+    # Get paths in $GAZEBO_MODEL_PATH
     model_paths = os.environ['GAZEBO_MODEL_PATH'].split(os.pathsep)
 
     # Iterate over objects and spawn
     for sod in data:
         # Define object pose
         object_pose = Pose()
-        object_pose.position.x = sod["x"]
-        object_pose.position.y = sod["y"]
-        object_pose.position.z = sod["z"]
+        object_pose.position.x = sod['x']
+        object_pose.position.y = sod['y']
+        object_pose.position.z = sod['z']
 
         # Search for model folder in $GAZEBO_MODEL_PATH
         for path in model_paths:
-            if os.path.isdir(path + '/' + sod["type"]):
-                model_path = path + '/' + sod["type"]
+            if os.path.isdir(path + '/' + sod['type']):
+                model_path = path + '/' + sod['type']
 
+        # Return error when folder could not be found
         try:
             model_path
         except NameError:
@@ -47,8 +48,11 @@ if __name__ == "__main__":
         if os.path.isfile(model_path + '/model.sdf'):
             sdf_model_path = model_path + '/model.sdf'
         else:
+            # If is no model.sdf and there are multiple sdf files, return the last
+            # alphabetically which is assumed to be for the highest sdf version.
             sdf_model_path = sorted(glob.glob(model_path + '/*.sdf'))[-1]
 
+        # Return error when no sdf file could be found
         try:
             sdf_model_path
         except NameError:
@@ -59,4 +63,4 @@ if __name__ == "__main__":
             sdf_file = f.read()
 
         # Spawn object
-        spawn_model_prox(sod["id"], sdf_file, "spawned_objects", object_pose, "world")
+        spawn_model_prox(sod['id'], sdf_file, 'spawned_objects', object_pose, 'world'')
