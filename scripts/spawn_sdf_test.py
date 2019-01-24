4b989db7aa34d6345bcc94f42315a78a417cfd7e
Kwin van der Veen <kwinvdv@gmail.com>
2019-01-24 12:53:01 +0100

    Update spawn_sdf_test.py
    Added comment about its functionality.

--- scripts/spawn_sdf_test.py
+++ scripts/spawn_sdf_test.py
@@ -2,4 +2,5 @@
 from ed_object_models import spawn_sdf_in_gazebo
 
 if __name__ == "__main__":
-    spawn_sdf_in_gazebo.from_yaml('/sdf_test/objects_list_test2.yaml')
+    # This spawns 7 coke cans, similar to fast_simulator_data/worlds/robotics_testlabs/robotics_testlabs.py
+    spawn_sdf_in_gazebo.from_yaml('/sdf_test/objects_list_test.yaml')
