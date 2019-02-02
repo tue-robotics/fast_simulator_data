33016c5959af576c3eba345b5a9af7290d0d8169
Kwin van der Veen <kwinvdv@gmail.com>
2019-02-02 15:49:43 +0100

    Updated to match the updated spawn_sdf_in_gazebo.from_yaml function, which now requires a full yaml file path and allows for orientations.

--- scripts/spawn_sdf_test.py
+++ scripts/spawn_sdf_test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 from ed_object_models import spawn_sdf_in_gazebo
+from rospkg import RosPack
 
 if __name__ == "__main__":
     # This spawns 7 coke cans, similar to fast_simulator_data/worlds/robotics_testlabs/robotics_testlabs.py
-    spawn_sdf_in_gazebo.from_yaml('/sdf_test/objects_list_test.yaml')
+    package_path = RosPack().get_path('fast_simulator_data')
+    spawn_sdf_in_gazebo.from_yaml(package_path + '/sdf_test/objects_list_test.yaml')
