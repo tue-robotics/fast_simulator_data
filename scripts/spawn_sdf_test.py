f9d5496f02dacf429227fc25d4a145dccfd90e42
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-03-10 12:22:52 +0100

    blaat

--- scripts/spawn_sdf_test.py
+++ scripts/spawn_sdf_test.py
@@ -2,6 +2,7 @@
 from ed_object_models import spawn_sdf_in_gazebo
 from rospkg import RosPack
 
+
 if __name__ == "__main__":
     # This spawns 7 coke cans, similar to fast_simulator_data/worlds/robotics_testlabs/robotics_testlabs.py
     package_path = RosPack().get_path('fast_simulator_data')
